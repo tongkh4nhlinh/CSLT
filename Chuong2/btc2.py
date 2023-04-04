fc0c13519ce1453b36a7ebbc72e5a0ea25fc3f82
Tong Khanh Linh <tongkh4nhlinh>
2023-04-04 09:16:48 +0700

    $ git push origin master

---
+++ Chuong2/btc2.py
@@ -0,0 +1,26 @@
+# 2.1
+# a=str(input('Ho ten: '))
+# b=int(input('Tuoi: '))
+# print('Chao ban', a+str(','),'Chuc mung sinh nhat lan thu',b,'!!!')
+# dk tai khoan, dong bo du lieu tu vs code
+
+# 2.2
+# a=int(input('Nhap Gia niem yet: '))
+# b=int(input('Nhap Chiet khau: '))
+# VAT=(a-b)*0.01
+# c=a-b+VAT
+# print('Gia ban: ',c)
+
+# 2.3
+# P=int(input('Tien dau tu ban dau: '))
+# n=int(input('So thang gui: '))
+# r=float(input('Lai suat tien gui moi thang: '))
+# print('Tien lanh cuoi ky: ', P*(1+r*n))
+
+# 2.4
+# a=int(input('a='))
+# b=int(input('b='))
+# c=int(input('c='))
+# S=(a+b+c)/2
+# import math
+# print('Dien tich=',math.sqrt(S*(S-a)*(S-b)*(S-c)))
---
+++ Chuong2/btcn2.py
@@ -0,0 +1,58 @@
+#2.1
+# a=str(input('Nhap ho ten: '))
+# print('Chao ban',a,'!!!')
+
+# 2.2
+# r=int(input('Nhap vao ban kinh cua duong tron: '))
+# pi=3.14
+# s=round(pi*r**2,1)
+# p=round(2*r*pi,1)
+# print('Dien tich cua duong tron co ban kinh',r,' la =',s)
+# print('Chu vi cua duong tron co ban kinh',r,' la =',p)
+
+# 2.3
+# a=int(input('Nhap hai canh ke cua tam giac vuong:'))
+# b=int(input())
+# import math
+# c=round(math.sqrt((a**2)+(b**2)),2)
+# print('Canh ke thu nhat la:',a,',canh ke thu hai:',b, 'co canh huyen=',c)
+
+# 2.4
+# a=int(input('a='))
+# b=int(input('b='))
+# c=int(input('c='))
+# d=int(input('d='))
+# print('Tong=',float(a+b+c+d),sep='')
+# print('Trung binh cong=',(a+b+c+d)/4,sep='')
+
+# 2.5
+# n=int(input('So tien ban dau: '))
+# k=int(input('So thang gui: '))
+# t=float(input('Lai suat/thang: '))
+# m=n*(1+k*t)
+# print('Voi so tien ban dau',n,'sau',k,'thang gui','lai suat',t,'/thang')
+# print('Thi so tien nhan duoc cuoi ky la:',m)
+
+# 2.6
+# a=str(input('Ho ten:'))
+# b=int(input('So ngay cong:'))
+# c=int(input('Don gia ngay cong:'))
+# d=float(input('He so phu cap:'))
+# e=int(input('Tam ung:'))
+# luong=c*b*d
+# thuclinh=luong-e
+# print('Nhan vien',a,', Co tien Luong=', luong,'Tam ung=',e,'va thuc linh=',thuclinh)
+# import math
+# print('Nhap hai canh ke cua tam giac vuong:')
+# canhke1=int(input())
+# canhke2=int(input())
+# canhhuyen=math.sqrt(canhke1*canhke1+canhke2*canhke2*)
+# print('Canh ke thu nhat la:',canhke1,end='')
+# print(', canh ke thu hai:',canhke2,end='')
+# print(', co canh huyen =',round(canhhuyen,2))
+n=int(input('So tien ban dau: '))
+k=int(input('So thang gui: '))
+t=float(input('Lai suat/ thang: '))
+m=n*(1+k*t)
+print('Voi so tien ban dau',n,', sau ',k,' thang gui, lai suat ',t,'/ thang',sep='')
+print('Thi so tien nhan duoc cuoi ky la: ',m)
